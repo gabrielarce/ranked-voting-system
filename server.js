7c65c5c543a2519a29f2c99c16ac04a27048f432
Gabe <gabrielarce3@gmail.com>
2022-11-20 00:46:29 -0800

    refactor vote tallying function

--- server.js
+++ server.js
@@ -17,8 +17,7 @@ app.use(express.json())
 //ROUTES
 app.get("/", async(req, res) => {
     const votes = await Vote.find({})
-    const winner = await calculateMost(votes)
-    console.log(votes)
+    const winner = calculateMost(votes)
     res.render('index', { votes, winner })
 })
 
@@ -36,67 +35,79 @@ app.post("/vote", async(req, res) => {
     }
 })
 
+
+
 app.listen(PORT, () => {
     console.log('listening on port 9999')
 })
 
 // Calculate the winner of the Election
 function calculateMost(votes) {
-    let numberOfVoters = votes.length
-    let rank = 1
+
+    //Object that keeps count of the candidates and count of votes.
     let candidates = {
         fire: 0,
         grass: 0,
         water: 0,
-    }
+    };
+
+    //Object that holds the candidate with over 50% of the vote. Winner will be held here. 
     let highest = {};
+
+    //Object that holds the rank1 candidate with the least amount of votes.
     let lowest = {};
 
-    for (i = 0; i < numberOfVoters; i++) {
-        candidates[votes[i]["rank1"]] += 1
-    }
+    //Tally the rank1 votes and add to the candidates object. 
+    for (i = 0; i < votes.length; i++) {
+        candidates[votes[i]["rank1"]]++
+    };
 
+    //Loop through the candidates object, if any candidate has more than half the votes, add them to the highest object
     for (const key in candidates) {
-        if (candidates[key] > numberOfVoters / 2) {
+        if (candidates[key] >= votes.length / 2) {
             highest[key] = candidates[key]
         }
-    }
+    };
 
-    let low = [...Object.values(candidates)].sort((a, b) => a - b)[0]
+    //What was the lowest number of votes.
+    let low = [...Object.values(candidates)].sort((a, b) => a - b)[0];
 
+    //Loop through the candidates object, if any candidate votes matches the lowest value, add them to lowest object
     for (const key in candidates) {
         if (candidates[key] == low) {
             lowest[key] = candidates[key]
         }
-    }
+    };
 
-    let secondChoice = votes.filter(vote => vote["rank1"] == Object.keys(lowest)[0])
+    // Select all the vote objects where the the rank1 choice got the least amount of votes. This array will be used to add the rank2 vote to the candidates object. 
+    let secondChoice = votes.filter(vote => Object.keys(lowest).includes(vote["rank1"]));
 
+    //If there is only one key/value pair in the highest object, return that object(winner!)
     if (Object.keys(highest).length == 1) {
         return highest
     } else {
+        //If no one has more than 50% of the vote yet, add the rank2 votes to candidates object.
         for (i = 0; i < secondChoice.length; i++) {
-            candidates[secondChoice[i]["rank2"]] += 1
+            candidates[secondChoice[i]["rank2"]]++
         }
+
+        //Loop through the candidates object, if any candidate has more than half the votes, add them to the highest object
         for (const key in candidates) {
-            if (candidates[key] > numberOfVoters / 2) {
+            if (candidates[key] >= votes.length / 2) {
                 highest[key] = candidates[key]
             }
         }
-        return highest
-    }
-}
+        console.log(highest)
+        if (Object.keys(highest).length == 1) {
+            return highest;
+        }
 
-// function calculateMost(votes) {
-//     let choices = 3
-//     const candidates = {
-//         fire: 0,
-//         grass: 0,
-//         water: 0,
-//     }
-//     for (i = 0; i < votes.length; i++) {
-//         let rank1 = votes[i].rank1
-//         candidates[rank1] += 1
-//     }
-//     return candidates
-// }
+        let newHighest = {}
+        for (const key in candidates) {
+            if (highest[key] >= Object.values(highest).reduce((acc, curr) => acc + curr) / 2) {
+                newHighest[key] = highest[key]
+            }
+        }
+        return newHighest;
+    };
+}
