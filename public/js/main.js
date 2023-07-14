29f55deb1e1adbee601f21abc5d8a47afc7e6e2c
Gabe <gabrielarce3@gmail.com>
2023-07-13 21:53:06 -0700

    swap out election

--- public/js/main.js
+++ public/js/main.js
@@ -1,42 +1,41 @@
-let radio1 = document.forms["frm1"].elements["rank1"];
-let inputs = [...document.querySelectorAll('input[type="radio"]')]
-let labels = [...document.querySelectorAll('input[name="rank1"] + label')]
+let radio1 = document.forms['frm1'].elements['rank1'];
+let inputs = [...document.querySelectorAll('input[type="radio"]')];
+let labels = [...document.querySelectorAll('input[name="rank1"] + label')];
 
 function displayWinner() {
-    let winner = document.querySelector(".winner");
-    winner.style.display = "block";
-    winner.scrollIntoView();
+  let winner = document.querySelector('.winner');
+  winner.style.display = 'block';
+  winner.scrollIntoView();
 }
 
-radio1.forEach(radio => {
-    radio.addEventListener("click", function() {
-        console.log(this.id, radio, inputs, labels)
-        inputs.forEach(input => {
-            input.disabled = false
-        })
+radio1.forEach((radio) => {
+  radio.addEventListener('click', function (event) {
+    console.log(this.id, radio, inputs, labels);
+    inputs.forEach((input) => {
+      input.disabled = false;
+    });
 
-        let type = event.target.value
-        document.getElementById(type + "2").disabled = true
-        document.getElementById(type + "3").disabled = true
-        document.querySelector(`#${this.id} + label`).classList.toggle("red")
+    let type = event.target.value;
+    console.log(type);
+    document.getElementById(type + '2').disabled = true;
+    document.getElementById(type + '3').disabled = true;
+  });
+});
 
-    })
-})
-
-var radios2 = document.forms["frm1"].elements["rank2"];
+var radios2 = document.forms['frm1'].elements['rank2'];
 for (radio in radios2) {
-    radios2[radio].onclick = function() {
-        let type = event.target.value
-        document.getElementById(type + "3").disabled = true
-    }
+  radios2[radio].onclick = function (event) {
+    let type = event.target.value;
+    document.getElementById(type + '3').disabled = true;
+  };
 }
 
 //Reset Form Button
 function resetForm() {
-    let inputs = document.querySelectorAll('input[type="radio"]')
-    let inputsArray = [...inputs]
-    inputsArray.forEach(input => {
-        input.disabled = false
-    })
-    document.getElementById("frm1").reset()
+  let inputs = document.querySelectorAll('input[type="radio"]');
+  let inputsArray = [...inputs];
+  inputsArray.forEach((input) => {
+    input.disabled = false;
+  });
+  document.getElementById('frm1').reset();
 }
--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const app = express();
-const path = require("path");
+const path = require('path');
 const PORT = process.env.PORT || 9999;
 const connectDB = require('./config/database');
-const Vote = require("./models/Votes");
-require("dotenv").config();
+const Vote = require('./models/Votes');
+require('dotenv').config();
 
 connectDB();
 
@@ -12,106 +12,110 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json())
+app.use(express.json());
 
 //ROUTES
-app.get("/", async(req, res) => {
-    const votes = await Vote.find({})
-    const winner = calculateMost(votes)
-    res.render('index', { votes, winner })
-})
+app.get('/', async (req, res) => {
+  const votes = await Vote.find({});
+  const winner = calculateMost(votes);
+  res.render('index', { votes, winner });
+});
 
-app.get("/createform", (req, res) => {
-    res.render("newElection")
-})
+app.get('/createform', (req, res) => {
+  res.render('newElection');
+});
 
-app.post("/vote", async(req, res) => {
-    try {
-        await Vote.create({
-            name: req.body.name,
-            rank1: req.body.rank1,
-            rank2: req.body.rank2,
-            rank3: req.body.rank3
-        })
-        res.redirect("/")
-    } catch (error) {
-        console.log(error)
-    }
-})
-
-
+app.post('/vote', async (req, res) => {
+  try {
+    await Vote.create({
+      name: req.body.name,
+      rank1: req.body.rank1,
+      rank2: req.body.rank2,
+      rank3: req.body.rank3,
+    });
+    const ipAddress = req.ip;
+    console.log('User IP Address:', ipAddress);
+    res.redirect('/');
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 app.listen(PORT, () => {
-    console.log('listening on port 9999')
-})
+  console.log('listening on port 9999');
+});
 
 // Calculate the winner of the Election
 function calculateMost(votes) {
+  //Object that keeps count of the candidates and count of votes.
+  let candidates = {
+    strawberry: 0,
+    mint: 0,
+    vanilla: 0,
+  };
 
-    //Object that keeps count of the candidates and count of votes.
-    let candidates = {
-        fire: 0,
-        grass: 0,
-        water: 0,
-    };
+  //Object that holds the candidate with over 50% of the vote. Winner will be held here.
+  let highest = {};
 
-    //Object that holds the candidate with over 50% of the vote. Winner will be held here. 
-    let highest = {};
+  //Object that holds the rank1 candidate with the least amount of votes.
+  let lowest = {};
 
-    //Object that holds the rank1 candidate with the least amount of votes.
-    let lowest = {};
+  //Tally the rank1 votes and add to the candidates object.
+  for (i = 0; i < votes.length; i++) {
+    candidates[votes[i]['rank1']]++;
+  }
 
-    //Tally the rank1 votes and add to the candidates object. 
-    for (i = 0; i < votes.length; i++) {
-        candidates[votes[i]["rank1"]]++
-    };
+  //Loop through the candidates object, if any candidate has more than half the votes, add them to the highest object
+  for (const key in candidates) {
+    if (candidates[key] >= votes.length / 2) {
+      highest[key] = candidates[key];
+    }
+  }
+
+  //What was the lowest number of votes.
+  let low = [...Object.values(candidates)].sort((a, b) => a - b)[0];
+
+  //Loop through the candidates object, if any candidate votes matches the lowest value, add them to lowest object
+  for (const key in candidates) {
+    if (candidates[key] == low) {
+      lowest[key] = candidates[key];
+    }
+  }
+
+  // Select all the vote objects where the the rank1 choice got the least amount of votes. This array will be used to add the rank2 vote to the candidates object.
+  let secondChoice = votes.filter((vote) =>
+    Object.keys(lowest).includes(vote['rank1'])
+  );
+
+  //If there is only one key/value pair in the highest object, return that object(winner!)
+  if (Object.keys(highest).length == 1) {
+    return highest;
+  } else {
+    //If no one has more than 50% of the vote yet, add the rank2 votes to candidates object.
+    for (i = 0; i < secondChoice.length; i++) {
+      candidates[secondChoice[i]['rank2']]++;
+    }
 
     //Loop through the candidates object, if any candidate has more than half the votes, add them to the highest object
     for (const key in candidates) {
-        if (candidates[key] >= votes.length / 2) {
-            highest[key] = candidates[key]
-        }
-    };
-
-    //What was the lowest number of votes.
-    let low = [...Object.values(candidates)].sort((a, b) => a - b)[0];
-
-    //Loop through the candidates object, if any candidate votes matches the lowest value, add them to lowest object
-    for (const key in candidates) {
-        if (candidates[key] == low) {
-            lowest[key] = candidates[key]
-        }
-    };
-
-    // Select all the vote objects where the the rank1 choice got the least amount of votes. This array will be used to add the rank2 vote to the candidates object. 
-    let secondChoice = votes.filter(vote => Object.keys(lowest).includes(vote["rank1"]));
-
-    //If there is only one key/value pair in the highest object, return that object(winner!)
+      if (candidates[key] >= votes.length / 2) {
+        highest[key] = candidates[key];
+      }
+    }
+    console.log(highest);
     if (Object.keys(highest).length == 1) {
-        return highest
-    } else {
-        //If no one has more than 50% of the vote yet, add the rank2 votes to candidates object.
-        for (i = 0; i < secondChoice.length; i++) {
-            candidates[secondChoice[i]["rank2"]]++
-        }
+      return highest;
+    }
 
-        //Loop through the candidates object, if any candidate has more than half the votes, add them to the highest object
-        for (const key in candidates) {
-            if (candidates[key] >= votes.length / 2) {
-                highest[key] = candidates[key]
-            }
-        }
-        console.log(highest)
-        if (Object.keys(highest).length == 1) {
-            return highest;
-        }
-
-        let newHighest = {}
-        for (const key in candidates) {
-            if (highest[key] >= Object.values(highest).reduce((acc, curr) => acc + curr) / 2) {
-                newHighest[key] = highest[key]
-            }
-        }
-        return newHighest;
-    };
+    let newHighest = {};
+    for (const key in candidates) {
+      if (
+        highest[key] >=
+        Object.values(highest).reduce((acc, curr) => acc + curr) / 2
+      ) {
+        newHighest[key] = highest[key];
+      }
+    }
+    return newHighest;
+  }
 }
